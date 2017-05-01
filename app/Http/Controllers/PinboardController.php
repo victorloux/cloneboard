f26e2af182cfe2b1028be72c027fce9427e8bffa
Victor <io@victorloux.uk>
2017-05-01 14:11:11 +0100

    Error logging for the Pinboard API

--- app/Http/Controllers/PinboardController.php
+++ app/Http/Controllers/PinboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use \kijin\PinboardAPI as PinboardAPI;
 
 class PinboardController extends Controller
@@ -11,7 +12,30 @@ class PinboardController extends Controller
 
     protected function connect()
     {
-        $this->pinboard = new \PinboardAPI(env('PINBOARD_USER'), env('PINBOARD_USER') . ':' . env('PINBOARD_TOKEN'));
+        try {
+            $this->pinboard = new \PinboardAPI(
+                env('PINBOARD_USER'),
+                env('PINBOARD_USER') . ':' . env('PINBOARD_TOKEN')
+            );
+        }
+        catch(PinboardException_ConnectionError $e) {
+            Log::error("Error connecting to the Pinboard API, likely due to an outage");
+        }
+        catch (PinboardException_AuthenticationFailure $e) {
+            Log::alert("Authentication failure. You might have the wrong token.");
+        }
+        catch(PinboardException_TooManyRequests $e) {
+            Log::warning("Rate limit was hit. Please wait a bit before trying again!");
+        }
+        catch(PinboardException_InvalidResponse $e) {
+            Log::error("Invalid response received", ['error' => $e]);
+        }
+        catch (PinboardException $e) {
+            Log::error("Error connecting to the Pinboard API", ['error' => $e]);
+            return false;
+        }
+
+        return true;
     }
 
     public function show()
