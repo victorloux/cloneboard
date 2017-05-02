d99e71984ab561ed30d97cb73cb548e763d41315
Victor <io@victorloux.uk>
2017-05-02 15:46:35 +0100

    Template specific to victorloux.uk

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -1,17 +1,24 @@
 <article>
-     <h3><a href="{{ $bookmark->url }}">{{ $bookmark->title }}</a></h3>
+    <aside>
+        <time>{{ $bookmark->time_posted->diffForHumans() }}</time>
+    </aside>
+    
+    <div class="definition">
+        <h4><a href="{{ $bookmark->url }}">{{ $bookmark->title }}</a></h4>
+        <h5><a href="{{ $bookmark->url }}"><strong>{{ $bookmark->rootUrl }}</strong>{{ parse_url($bookmark->url, PHP_URL_PATH) }}</a></h5>
 
-     <div class="description">
-         {{ $bookmark->description }}
-     </div>
-
-
-     <ul class="tags">
-         @foreach($bookmark->tags as $tag)
-             <?php if($tag == 'via:popular') continue; ?>
-             <li><a href="{{ action("BookmarkController@showTag", ['tag' => $tag->tag ]) }}">{{ $tag }}</a></li>
-         @endforeach
-     </ul>
-     <div class="url"><a href="{{ $bookmark->url }}">{{ $bookmark->rootUrl }}</a> &middot; {{ $bookmark->time_posted->diffForHumans() }}</div>
-
+        @if(!empty($bookmark->description))
+            <p class="details">
+                {{ $bookmark->description }}
+            </p>
+        @endif
+        
+        <ul class="tags">
+            <li>Tags</li>
+            @foreach($bookmark->tags as $tag)
+                <?php if($tag == 'via:popular') continue; ?>
+                 <li><a href="{{ action("BookmarkController@showTag", ['tag' => $tag->tag ]) }}">{{ $tag}}</a></li>
+            @endforeach
+        </ul>
+    </div>
 </article>
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,29 +5,50 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <script src="//use.typekit.net/tik4qco.js"></script>
+        <script type="text/javascript">
+            try{Typekit.load();}catch(e){}
+        </script>
+        <link rel="stylesheet" href="{{ env("CSS_ROOT", "css/master.css") }}">
+
         <title>Victor Loux — bookmarks</title>
     </head>
     <body>
-        <form action="{{ action("BookmarkController@searchForm") }}" id="search" method="POST">
-            <input type="text" name="query" value="{{ old('query') }}">
-            <input type="submit" name="search" value="search">
-            {{ csrf_field() }}
-        </form>
+        <header>
+            <h1><a href="/">Victor Loux</a> <span class="separator">&rarr;</span> Bookmarks</h1>
+        </header>
+        
+        <section class="intro">
+            <p class="interests">
+                A feed of <strong>interesting articles and websites</strong> I have recently read.
+                Please feel free to browse and <a href="mailto:io@victorloux.uk">send me suggestions</a> of sites to look at — I’m curious about a lot of things!
+            </p>
+        </section>
+
 
         @if(isset($tagName))
             <h2>Bookmarks tagged <strong>{{ $tagName }}</strong></h2>
         @elseif(isset($query))
-            <h2>Searching for “<strong>{{ $query }}</strong>”</h2>
-            <p>{{ $resultsCount }} results.</p>
+            <h2>{{ $resultsCount }} results for “<strong>{{ $query }}</strong>”</h2>
         @endif
 
-        <section class="bookmarks">
+        <section class="cv bookmarks">
            @foreach($bookmarks as $bookmark)
                @include("bookmark")
             @endforeach
         </section>
         <section class="pagination">
             {{ $bookmarks->links() }}
+        </section>
+        
+        <section class="extra">
+            <p>{{ number_format($totalBookmarks) }} bookmarks total &middot; Imported from <a href="https://pinboard.in/u:vloux">Pinboard</a></p>
+            
+            <form action="{{ action("BookmarkController@searchForm") }}" id="search" method="POST">
+                <input type="text" name="query" value="{{ old('query') }}"><input type="submit" name="search" value="search">
+                {{ csrf_field() }}
+            </form>
+            </p>
         </section>
     </body>
 </html>
