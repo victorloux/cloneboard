c8f4d1bdcc440fc895d0acbfc86ebe85ed06d013
Victor <io@victorloux.uk>
2017-05-02 10:43:50 +0100

    Properly generate URL to the tag view

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -24,7 +24,7 @@
                     <ul class="tags">
                         @foreach($bookmark->tags as $tag)
                             <?php if($tag == 'via:popular') continue; ?>
-                            <li><a href="/tag/{{ $tag }}">{{ $tag }}</a></li>
+                            <li><a href="{{ action("BookmarkController@showTag", ['tag' => $tag->tag ]) }}">{{ $tag }}</a></li>
                         @endforeach
                     </ul>
                     <div class="url"><a href="{{ $bookmark->url }}">{{ $bookmark->rootUrl }}</a> &middot; {{ $bookmark->time_posted->diffForHumans() }}</div>
