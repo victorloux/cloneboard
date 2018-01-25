9ac434a3cf88407b9108e8a6a64a0a6ff08c15fb
Victor <io@victorloux.uk>
2018-01-25 23:33:10 +0000

    Updated templates for new CSS

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -1,12 +1,13 @@
 <article class="row">
-    <aside class="two columns aside">
-        <time datetime="{{ $bookmark->time_posted->format("j F Y H:i") }}" title="{{ $bookmark->time_posted->format("j F Y H:i") }}">{{ $bookmark->time_posted->diffForHumans() }}</time>
-    </aside>
     
-    <div class="eight columns definition">
-        <h4><a href="{{ $bookmark->url }}">{{ $bookmark->title }}</a></h4>
-
-        <a href="{{ $bookmark->url }}" class="link"><strong>{{ $bookmark->rootUrl }}</strong>{{ parse_url($bookmark->url, PHP_URL_PATH) }}</a>
+    <time datetime="{{ $bookmark->time_posted->format("j F Y H:i") }}" title="{{ $bookmark->time_posted->format("j F Y H:i") }}">{{ $bookmark->time_posted->diffForHumans() }}</time>
+    
+    
+    <div class="definition">
+        <a href="{{ $bookmark->url }}">
+            <h2>{{ $bookmark->title }}</h2>
+            <span class="url"><strong>{{ $bookmark->rootUrl }}</strong>{{ parse_url($bookmark->url, PHP_URL_PATH) }}</span>
+        </a>
 
         @if(!empty($bookmark->description))
             <p class="details">
@@ -14,12 +15,14 @@
             </p>
         @endif
         
-        <ul class="tags">
-            <li>Tags</li>
-            @foreach($bookmark->tags as $tag)
-                <?php if($tag == 'via:popular') continue; ?>
-                 <li><a href="{{ action("BookmarkController@showTag", ['tag' => $tag->tag ]) }}">{{ $tag}}</a></li>
-            @endforeach
-        </ul>
+        @if(count($bookmark->tags) > 1)
+            <ul class="tags">
+                <li><i data-feather="tag"><span class="sr-only">Tag</span></i></li>
+                @foreach($bookmark->tags as $tag)
+                    <?php if($tag == 'via:popular') continue; ?>
+                     <li><a href="{{ action("BookmarkController@showTag", ['tag' => $tag->tag ]) }}">{{ $tag }}<span class="sr-only">, tag</a></li>
+                @endforeach
+            </ul>
+        @endif
     </div>
 </article>
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,70 +5,118 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="//use.typekit.net/tik4qco.js"></script>
-        <script type="text/javascript">
-            try{Typekit.load();}catch(e){}
-        </script>
         <link rel="stylesheet" href="{{ env("CSS_ROOT", "css/master.css") }}">
 
-        <title>Victor Loux — bookmarks</title>
+        <title>Victor Loux — bookmarks
+            @if(isset($tagName))
+                tagged “{{ $tagName }}”
+            @elseif(isset($query))
+                — Search: “{{ $query }}”
+            @endif
+        </title>
+        
+        {{-- <script type="text/javascript">
+            document.addEventListener("DOMContentLoaded", function() {
+                class list {
+                    constructor(domElement) {
+                        this.tags = domElement.querySelectorAll("a");
+                        this.makeFirstSelectable();
+                        this.maxIndex = tags.length;
+                    }
+                    
+                    makeFirstSelectable() {
+                        this.tags.forEach(function(tag, idx) {
+                            if(idx == 0) return;
+                            tag.tabIndex = -1;
+                        });
+                        this.currentIndex = 0;
+                    }
+                }
+                let lists = [];
+
+                let listsInDOM = document.querySelectorAll(".tags");
+                listsInDOM.forEach(function(el) {
+                    lists.push(new list(el))
+                });
+                
+                // let selectableTags = document.querySelectorAll(".tags a:not([tabindex='-1'])");
+                // selectableTags.forEach(function(el) {
+                //     el.addEventListener("focus", function() {
+                //         // get current index, total indices
+                //         console.log(this.innerHTML);
+                //         let ul = this.parentElement.parentElement;
+                //         ul.querySelectorAll("li");
+                //     });
+                // });
+            })
+        </script> --}}
     </head>
-    <body>
+    <body class="bookmarks">
         <div class="container">
-            <header>
-                <h1 class="eight columns offset-by-two">
-                    <a href="/">Victor Loux</a> <span class="separator">&rarr;</span>
+            <header class="row">
+                <h1>
+                    <a href="/">Victor Loux</a>
+                    <span class="separator">&rarr;</span>
                     @if(isset($tagName) || isset($query))
                         <a href="/bookmarks/">Bookmarks</a>
+                        <span class="separator">&rarr;</span>
+                        @if(isset($tagName))
+                            <i data-feather="tag"><span class="sr-only">Tag</span></i> {{ $tagName }}
+                        @elseif(isset($query))
+                            <i data-feather="search"></i> Search: “{{ $query }}”
+                        @endif
                     @else
                         Bookmarks
                     @endif
                 </h1>
             
-                <section class="row intro">
-                    <p class="interests seven columns offset-by-two">
-                        A feed of <strong>interesting articles and websites</strong> I have recently read.
-                        Please feel free to browse and <a href="mailto:io@victorloux.uk">send me suggestions</a> of sites to look at — I’m curious about a lot of things!
-                    </p>
-                </section>
+                <p class="intro row">
+                @if(isset($tagName))
+                    @if($resultsCount == 0)
+                        No results for {{ $tagName }}.
+                    @else
+                        {{ $resultsCount }} {{ str_plural("bookmark", $resultsCount) }} tagged “<strong>{{ $tagName }}</strong>”
+                    @endif
+                @elseif(isset($query))
+                    @if($resultsCount == 0)
+                        No results for <strong>{{ $query }}</strong>.
+                    @else
+                        {{ $resultsCount }} {{ str_plural("result", $resultsCount) }} results for “<strong>{{ $query }}</strong>”
+                    @endif
+                @else
+                    A feed of <strong>interesting articles and websites</strong> I have recently read.
+                    Please feel free to browse and <a href="mailto:io@victorloux.uk">send me suggestions</a> of sites to look at — I’m curious about a lot of things!
+                @endif
+                </p>
+                
+                <form action="{{ action("BookmarkController@searchForm") }}" method="POST" role="search">
+                    <input type="text" name="query" value="{{ old('query') }}" placeholder="Find a tag or keyword&hellip;" aria-label="Search for a tag or keyword"><input type="submit" name="search" value="Search">
+                    {{ csrf_field() }}
+                </form>
             </header>
 
-
-            @if(isset($tagName))
-                @if($resultsCount == 0)
-                    <h2 class="eight columns offset-by-two">No results for {{ $tagName }}!</h2>
-                @else
-                    <h2 class="eight columns offset-by-two">{{ $resultsCount }} {{ str_plural("bookmark", $resultsCount) }} tagged “<strong>{{ $tagName }}</strong>”</h2>
-                @endif
-            @elseif(isset($query))
-                @if($resultsCount == 0)
-                    <h2 class="eight columns offset-by-two">No results for {{ $query }}!</h2>
-                @else
-                    <h2 class="eight columns offset-by-two">{{ $resultsCount }} {{ str_plural("result", $resultsCount) }} results for “<strong>{{ $query }}</strong>”</h2>
-                @endif
-            @endif
-            
             @if($bookmarks->currentPage() > 1)
                 <div class="row eight columns offset-by-two">{{ $bookmarks->links() }}</div>
             @endif
 
-            <section class="cv bookmarks">
+            <main class="cv bookmarks">
                @foreach($bookmarks as $bookmark)
                    @include("bookmark")
                 @endforeach
-            </section>
+            </main>
 
             <div class="row eight columns offset-by-two">{{ $bookmarks->links() }}</div>
             
-            <footer class="extra eight columns offset-by-two">
-                <p>{{ number_format($totalBookmarks) }} bookmarks total &middot; Imported from <a href="https://pinboard.in/u:vloux">Pinboard</a></p>
-                
-                <form action="{{ action("BookmarkController@searchForm") }}" class="search" method="POST" aria-role="search">
-                    <input type="text" name="query" value="{{ old('query') }}" placeholder="Tag or keyword"><input type="submit" name="search" value="search">
-                    {{ csrf_field() }}
-                </form>
-                </p>
+            <footer>
+                {{ number_format($totalBookmarks) }} bookmarks total &middot; Imported from <a href="https://pinboard.in/u:vloux">Pinboard</a>
             </footer>
         </div>
+        
+        {{-- @TODO: should probably just use real SVGs. --}}
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+        <script>
+          feather.replace()
+        </script>
+
     </body>
 </html>
